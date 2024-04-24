0707519a8d3eaa10264c2fe9d4de5aaf2bba2a91
OthmanAlkilany <otmanutm@gmail.com>
2024-04-24 12:00:49 +0200

    رسالة تنبيه

--- WinFormsApp1/Form1.vb
+++ WinFormsApp1/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Button1.Text = "حفظ التغييرات"
+        MsgBox("عذرا ...تاكد من ادخال البيانات", MsgBoxStyle.Exclamation)
     End Sub
 End Class
