1c82acf2ae767bc4bb43614fe57f6a3d14996c37
OthmanAlkilany <otmanutm@gmail.com>
2024-04-24 11:53:22 +0200

    انشاء الواجهة الاولى

--- WinFormsApp1/Form1.Designer.vb
+++ WinFormsApp1/Form1.Designer.vb
@@ -22,10 +22,62 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        ComboBox1 = New ComboBox()
+        RadioButton1 = New RadioButton()
+        ProgressBar1 = New ProgressBar()
+        DateTimePicker1 = New DateTimePicker()
+        SuspendLayout()
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(546, 84)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(151, 28)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(525, 343)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(121, 24)
+        RadioButton1.TabIndex = 1
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "RadioButton1"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' ProgressBar1
+        ' 
+        ProgressBar1.Location = New Point(177, 264)
+        ProgressBar1.Name = "ProgressBar1"
+        ProgressBar1.Size = New Size(125, 29)
+        ProgressBar1.TabIndex = 2
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(163, 122)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(250, 27)
+        DateTimePicker1.TabIndex = 3
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(ProgressBar1)
+        Controls.Add(RadioButton1)
+        Controls.Add(ComboBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
 
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents DateTimePicker1 As DateTimePicker
 End Class
